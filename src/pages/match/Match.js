1de139e86cd383d56595d97ce99f41107a5f4bf0
leandrolog <leandro.oscargn@gmail.com>
2023-11-09 21:45:10 -0300

    commit do match

---
+++ src/pages/match/Match.js
@@ -0,0 +1,43 @@
+import './match.css'
+import CardBox from "../../components/card/CardBox";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import MatchModal from "../../components/modal/MatchModal";
+
+
+function Match() {
+
+    const [data, setData] = useState();
+
+    const getMatch = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/match')
+            setData(response.data)
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
+    useEffect(() => {
+        getMatch()
+    }, [])
+    return (
+        <div>
+            <div className="match-header">
+                <h1>Match page</h1>
+            </div>
+            <div className="modal-button">
+                <MatchModal
+                    data={getMatch}
+                    modalTitle="Adicionar"
+                />
+            </div>
+            <div className="match-content">
+                    <CardBox data={data}/>
+            </div>
+        </div>
+
+    )
+}
+
+export default Match
